3a5a10f3f561d6888beb5b98c100cf79693da7d7
janghos <seojh7816@gmail.com>
2024-10-09 15:35:31 +0900

    [init] FoodiLog 프로젝트 시작

---
+++ app/src/main/java/com/foodilog/ApiClient.kt
@@ -0,0 +1,18 @@
+package com.foodilog
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.Body
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface ApiClient {
+
+}
---
+++ app/src/main/java/com/foodilog/FoodilogApplication.kt
@@ -0,0 +1,7 @@
+package com.foodilog
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class FoodilogApplication : Application() {}
