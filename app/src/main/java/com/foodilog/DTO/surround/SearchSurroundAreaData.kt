9699fbbde916cebdd33a2735142a4dd8e4908f88
janghos <seojh7816@gmail.com>
2024-10-09 16:01:37 +0900

    [feat] data class Serializable implement

--- app/src/main/java/com/foodilog/DTO/surround/SearchSurroundAreaData.kt
+++ app/src/main/java/com/foodilog/DTO/surround/SearchSurroundAreaData.kt
@@ -2,11 +2,12 @@ package com.foodilog.DTO.surround
 
 
 import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 
 @Serializable
 data class SearchSurroundAreaData(
     @SerialName("html_attributions")
-    val htmlAttributions: List<Any>,
+    val htmlAttributions: List<String>,
     @SerialName("results")
     val results: List<Result>,
     @SerialName("status")
--- app/src/main/java/com/foodilog/DTO/surround/Viewport.kt
+++ app/src/main/java/com/foodilog/DTO/surround/Viewport.kt
@@ -2,6 +2,7 @@ package com.foodilog.DTO.surround
 
 
 import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 
 @Serializable
 data class Viewport(
